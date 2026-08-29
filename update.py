f263aa419f2c852ff1db8f0db09b2e1b386d5cfb
KB-Home <kheamboat@gmail.com>
2026-08-29 22:22:58 +0700

    Update update.py

--- update.py
+++ update.py
@@ -27,14 +27,22 @@ def generate_w3u():
     w3u_data = {
         "name": "ATMFLIX Live Sports",
         "author": "Auto Token Bot",
-        "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36",
-        "referer": "https://atmflix.live/",
         "station": [
             {
                 "name": "MonoMax 3",
                 "image": "https://drive.google.com/uc?export=download&id=1XWfSp-4LeKJAGbII5uVYAnDB3oZHyRST",
                 "url": f"https://cdn2.stream.atmflix.live/atmflixlive/monomax-sports3/chunks.m3u8?{token_query}",
-                "info": "atmflix.live"
+                "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36",
+                "info": "atmflix.live",
+                "referer": "https://atmflix.live/",
+            },
+            {
+                "name": "MonoMax 4",
+                "image": "https://drive.google.com/uc?export=download&id=1XWfSp-4LeKJAGbII5uVYAnDB3oZHyRST",
+                "url": f"https://cdn2.stream.atmflix.live/atmflixlive/monomax-sports4/chunks.m3u8?{token_query}",
+                "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36",
+                "info": "atmflix.live",
+                "referer": "https://atmflix.live/",
             }
         ]
     }
