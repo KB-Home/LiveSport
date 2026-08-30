d12da18694640c39e466c361d0f734fddef89cec
KB-Home <kheamboat@gmail.com>
2026-08-30 19:51:21 +0700

    update.py

--- update.py
+++ update.py
@@ -3,12 +3,13 @@ import json
 import requests
 
 def get_live_token():
-    web_url = "https://atmflix.live" 
+    web_url = "https://doolive4k.tv/ดูทีวีออนไลน์" 
     
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36',
-        'Referer': 'https://atmflix.live/',
-        'Origin': 'https://atmflix.live',
+        'Referer': 'https://doolive4k.tv/',
+        'Origin': 'https://doolive4k.tv',
+        'Host': 'kt5kce43ak.ja-me-kai-kai-rak-kon-na-ta-yang-chan.sbs',
         'Accept-Language': 'th,en-US;q=0.9,en;q=0.8'
     }
     try:
@@ -26,25 +27,23 @@ def get_live_token():
     except Exception as e:
         print(f"Error fetching token: {e}")
         
-    return "nimblesessionid=20666499&wmsAuthSign=c2VydmVyX3RpbWU9OC8yOS8yMDI2IDU6NTA6MzcgUE0maGFzaF92YWx1ZT16elhKUEU3K08wM0pIODJlakFobkVBPT0mdmFsaWRtaW51dGVzPTIw"
+    return "nimblesessionid=7843036&wmsAuthSign=c2VydmVyX3RpbWU9OC8zMC8yMDI2IDc6MjM6MzEgQU0maGFzaF92YWx1ZT01alhlcmExZlJQWTY4aHYvd29MckZ3PT0mdmFsaWRtaW51dGVzPTM2MCZpZD0x"
 
 def generate_w3u():
     token_query = get_live_token()
     w3u_data = {
-        "name": "ATMFLIX Sports",
+        "name": "doolive4k",
         "author": "Auto Token Bot",
         "station": [
             {
-                "name": "MonoMax 3",
-                "image": "https://atmflix.live/logo-square.png",
-                "url": f"https://cdn2.stream.atmflix.live/atmflixlive/monomax-sports3/chunks.m3u8?{token_query}",
-                "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36",
-                "referer": "https://atmflix.live/",
-                "info": "atmflix.live"
+                "name": "Bein 1",
+                "image": "https://drive.google.com/uc?export=download&id=1st_OJB-u_X125waLX27KQ3WBt744wB-V",
+                "url": f"https://kt5kce43ak.ja-me-kai-kai-rak-kon-na-ta-yang-chan.sbs/doolive4k-tv/bein-1-th/chunks.m3u8?{token_query}",
+                "info": "doolive4k.tv"
             }
         ]
     }
-    with open("ATMFlix.w3u", "w", encoding="utf-8") as f:
+    with open("doolive4k.w3u", "w", encoding="utf-8") as f:
         json.dump(w3u_data, f, ensure_ascii=False, indent=2)
     print("Playlist updated successfully!")
 
